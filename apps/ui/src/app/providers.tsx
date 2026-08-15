9ded3b1132c1673220ef689a36e5f057d992c141
Dennis Macharia <dennismacharia4747@gmail.com>
2026-08-15 15:12:34 +0300

    chore: update toaster position

--- apps/ui/src/app/providers.tsx
+++ apps/ui/src/app/providers.tsx
@@ -12,7 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <QueryClientProvider client={queryClient}>
         {children}
-        <Toaster />
+        <Toaster position="top-right" richColors />
       </QueryClientProvider>
     </ThemeProvider>
   );
